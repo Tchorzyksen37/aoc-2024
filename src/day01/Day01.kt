7d3e399d637eb3693d3488c0abd60fc7bf7bc7c3
Michal Tchorzewski <tchorzyksen@gmail.com>
2024-12-11 22:33:26 +0100

    :dizzy: Solve Day One

---
+++ src/day01/Day01.kt
@@ -0,0 +1,75 @@
+import kotlin.math.abs
+
+fun main() {
+
+    fun MutableList<Int>.binaryAdd(num: Int) {
+        val index = this.binarySearch(num)
+        if (index < 0)
+            this.add(index.inv(), num)
+        else
+            this.add(index, num)
+    }
+
+    fun part1(input: List<String>): Int {
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+
+        for (i in input) {
+            val inputSplit = i.split(" ").filter { it.isNotBlank() }
+
+            inputSplit[0].trim().toInt().let { leftList.binaryAdd(it) }
+            inputSplit[1].trim().toInt().let { rightList.binaryAdd(it) }
+        }
+
+        return leftList.zip(rightList).sumOf { (left, right) -> abs(left - right) }
+
+    }
+
+    fun oneGoldStar(input: List<String>): Int {
+        val (left, right) = input.map { line ->
+            val first = line.substringBefore(" ").toInt()
+            val second = line.substringAfterLast(" ").toInt()
+            first to second
+        }.unzip()
+
+        return left.sorted().zip(right.sorted()).sumOf { (left, right) -> abs(left - right) }
+    }
+
+    fun part2(input: List<String>): Int {
+        val leftList = mutableListOf<Int>()
+        val rightMap = mutableMapOf<Int, Int>()
+
+        for (i in input) {
+            val inputSplit = i.split(" ").filter { it.isNotBlank() }
+
+            inputSplit[0].trim().toInt().let { leftList.binaryAdd(it) }
+            inputSplit[1].trim().toInt().let {
+                if (rightMap.containsKey(it))
+                    rightMap[it] = rightMap[it]!! + 1
+                else
+                    rightMap.put(it, 1)
+            }
+        }
+
+        return leftList.sumOf { left -> rightMap.getOrDefault(left, 0) * left }
+    }
+
+    fun twoGoldStars(input: List<String>): Int {
+        val (left, right) = input.map { line -> line.split(Regex("\\s+")).let { it[0].toInt() to it[1].toInt() } }
+            .unzip()
+
+        val frequencies = right.groupingBy { it }.eachCount()
+        return left.sumOf { frequencies.getOrDefault(it, 0) * it }
+    }
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("day01/day01_test").filter { it.isNotBlank() }
+    check(oneGoldStar(testInput) == 11)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("day01/day01")
+    oneGoldStar(input).println()
+
+    check(twoGoldStars(testInput) == 31)
+    twoGoldStars(input).println()
+}
